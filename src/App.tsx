f0f0c3dbb9b9c111ef52b2c317c17fa6a353d868
prushton2 <peterrushton418@gmail.com>
2023-10-12 13:39:22 -0400

    working on embed data

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import steamLogo from '/steam2.png'
+import steamLogo from '/steam2.png';
+import {Helmet} from "react-helmet";
 import './App.css'
 
 function App() {
 
   return (
-    <head>
+    <p>Hi!
 
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://stxamcommunity.com"/>
-      <meta property="og:title" content="stxam"/>
-      <meta property="og:description" content=""/>
-      <meta property="og:image" content={steamLogo}/>
 
-    </head>
+      <Helmet>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://stxamcommunity.com"/>
+        <meta property="og:title" content="stxam"/>
+        <meta property="og:description" content=""/>
+        <meta property="og:image" content={steamLogo}/>
+      </Helmet>
+    </p>
   )
 }
 
