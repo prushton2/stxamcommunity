9e4be1f05f639c372d1710a48fd3a97c41e938a5
prushton2 <peterrushton418@gmail.com>
2023-10-12 12:19:13 -0400

    Uploaded better project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  document.title = "prushton.com";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>
+            Peter Rushton <br />
+          </p>
+
+          <p>
+            UMass Lowell Computer Science Student <br />
+          </p>
+
+          <p>
+            <a href="https://github.com/prushton2" style={{color: "grey"}}>Github</a>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
