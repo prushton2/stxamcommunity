5a3c689b1980c8034ef5a094f031edd55a009d88
prushton2 <peterrushton418@gmail.com>
2023-10-13 10:18:46 -0400

    moved stuff to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import steamLogo from '/steam2.png';
+// import steamLogo from '/steam2.png';
 import {Helmet} from "react-helmet";
 import './App.css'
 
@@ -9,11 +9,7 @@ function App() {
 
 
       <Helmet>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://stxamcommunity.com"/>
-        <meta property="og:title" content="stxam"/>
-        <meta property="og:description" content=""/>
-        <meta property="og:image" content={steamLogo}/>
+
       </Helmet>
     </p>
   )
